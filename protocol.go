857d8241598e34ef3b2b9cddbada9d370148a8ff
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-03 20:04:26 +0900

    protocol: remove deadcode

--- protocol.go
+++ protocol.go
@@ -3,21 +3,3 @@
 // license that can be found in the LICENSE file.
 
 package protocol
-
-import (
-	"log"
-)
-
-// unhandledError is used in places where an error may occur that cannot be handled.
-// This occurs in things like rpc handlers that are a notify, where we cannot
-// reply to the caller, or in a call when we are actually attempting to reply.
-// In these cases, there is nothing we can do with the error except log it, so
-// we do that in this function, and the presence of this function acts as a
-// useful reminder of why we are effectively dropping the error and also a
-// good place to hook in when debugging those kinds of errors.
-func unhandledError(err error) {
-	if err == nil {
-		return
-	}
-	log.Printf("%v", err)
-}
