03e6a9dc03468b186ca006a60838107bb7188ada
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-09 01:53:04 +0900

    types: add Uint64 function

---
+++ types.go
@@ -0,0 +1,9 @@
+// Copyright 2019 The go-language-server Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package protocol
+
+func Uint64(i uint64) *uint64 {
+	return &i
+}
