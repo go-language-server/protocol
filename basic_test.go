5c4c6cb3e1cbefecb05814dda4efb4d8244c2eac
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-08 21:21:59 +0900

    basic: add LocationLink testcases

--- basic_test.go
+++ basic_test.go
@@ -287,3 +287,109 @@ func TestLocation(t *testing.T) {
 		}
 	})
 }
+
+func TestLocationLink(t *testing.T) {
+	t.Run("Marshal", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name           string
+			field          LocationLink
+			want           string
+			wantMarshalErr bool
+			wantErr        bool
+		}{
+			{
+				name:           "Valid",
+				field:          LocationLink{OriginSelectionRange: &Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetURI: "file:///path/to/test.go", TargetRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetSelectionRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}},
+				want:           `{"originSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetUri":"file:///path/to/test.go","targetRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}}}`,
+				wantMarshalErr: false,
+				wantErr:        false,
+			},
+			{
+				name:           "ValidNilOriginSelectionRange",
+				field:          LocationLink{TargetURI: "file:///path/to/test.go", TargetRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetSelectionRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}},
+				want:           `{"targetUri":"file:///path/to/test.go","targetRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}}}`,
+				wantMarshalErr: false,
+				wantErr:        false,
+			},
+			{
+				name:           "Invalid",
+				field:          LocationLink{OriginSelectionRange: &Range{Start: Position{Line: 25, Character: 1}}, TargetURI: "file:///path/to/test.go", TargetRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetSelectionRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}},
+				want:           `{"originSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetUri":"file:///path/to/test.go","targetRange":{"start":{"line":2,"character":1},"end":{"line":3,"character":2}},"targetSelectionRange":{"start":{"line":2,"character":1},"end":{"line":3,"character":2}}}`,
+				wantMarshalErr: false,
+				wantErr:        true,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				got, err := gojay.MarshalJSONObject(&tt.field)
+				if (err != nil) != tt.wantMarshalErr {
+					t.Error(err)
+					return
+				}
+
+				if diff := cmp.Diff(string(got), tt.want); (diff != "") != tt.wantErr {
+					t.Errorf("%s: wantErr: %t\n(-got, +want)\n%s", tt.name, tt.wantErr, diff)
+				}
+			})
+		}
+	})
+
+	t.Run("Unmarshal", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name             string
+			field            string
+			want             LocationLink
+			wantUnmarshalErr bool
+			wantErr          bool
+		}{
+			{
+				name:             "Valid",
+				field:            `{"originSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetUri":"file:///path/to/test.go","targetRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}}}`,
+				want:             LocationLink{OriginSelectionRange: &Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetURI: "file:///path/to/test.go", TargetRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetSelectionRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}},
+				wantUnmarshalErr: false,
+				wantErr:          false,
+			},
+			{
+				name:             "ValidNilOriginSelectionRange",
+				field:            `{"targetUri":"file:///path/to/test.go","targetRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}}}`,
+				want:             LocationLink{TargetURI: "file:///path/to/test.go", TargetRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetSelectionRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}},
+				wantUnmarshalErr: false,
+				wantErr:          false,
+			},
+			{
+				name:             "Invalid",
+				field:            `{"originSelectionRange":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"targetUri":"file:///path/to/test.go","targetRange":{"start":{"line":2,"character":1},"end":{"line":3,"character":2}},"targetSelectionRange":{"start":{"line":2,"character":1},"end":{"line":3,"character":2}}}`,
+				want:             LocationLink{OriginSelectionRange: &Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetURI: "file:///path/to/test.go", TargetRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}, TargetSelectionRange: Range{Start: Position{Line: 25, Character: 1}, End: Position{Line: 27, Character: 3}}},
+				wantUnmarshalErr: false,
+				wantErr:          true,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				got := LocationLink{}
+				dec := gojay.BorrowDecoder(strings.NewReader(tt.field))
+				defer dec.Release()
+				if err := dec.Decode(&got); (err != nil) != tt.wantUnmarshalErr {
+					t.Error(err)
+					return
+				}
+
+				if diff := cmp.Diff(got, tt.want); (diff != "") != tt.wantErr {
+					t.Errorf("%s: wantErr: %t\n(-got, +want)\n%s", tt.name, tt.wantErr, diff)
+				}
+			})
+		}
+	})
+}
