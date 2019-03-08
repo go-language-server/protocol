5d4e5e2c94a46ebb9b43c8311483234adc7fede9
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-09 01:45:30 +0900

    basic: add TextDocumentEdit testcases

--- basic_test.go
+++ basic_test.go
@@ -1525,3 +1525,183 @@ func TestTextEdit(t *testing.T) {
 		}
 	})
 }
+
+func TestTextDocumentEdit(t *testing.T) {
+	t.Run("Marshal", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name           string
+			field          TextDocumentEdit
+			want           string
+			wantMarshalErr bool
+			wantErr        bool
+		}{
+			{
+				name: "Valid",
+				field: TextDocumentEdit{
+					TextDocument: VersionedTextDocumentIdentifier{
+						TextDocumentIdentifier: TextDocumentIdentifier{
+							URI: "file:///path/to/basic.go",
+						},
+						Version: Uint64(10),
+					},
+					Edits: []TextEdit{
+						{
+							Range: Range{
+								Start: Position{
+									Line:      25,
+									Character: 1,
+								},
+								End: Position{
+									Line:      27,
+									Character: 3,
+								},
+							},
+							NewText: "foo bar",
+						},
+					},
+				},
+				want:           `{"textDocument":{"uri":"file:///path/to/basic.go","version":10},"edits":[{"range":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"newText":"foo bar"}]}`,
+				wantMarshalErr: false,
+				wantErr:        false,
+			},
+			{
+				name: "Invalid",
+				field: TextDocumentEdit{
+					TextDocument: VersionedTextDocumentIdentifier{
+						TextDocumentIdentifier: TextDocumentIdentifier{
+							URI: "file:///path/to/basic.go",
+						},
+						Version: Uint64(10),
+					},
+					Edits: []TextEdit{
+						{
+							Range: Range{
+								Start: Position{
+									Line:      25,
+									Character: 1,
+								},
+								End: Position{
+									Line:      27,
+									Character: 3,
+								},
+							},
+							NewText: "foo bar",
+						},
+					},
+				},
+				want:           `{"textDocument":{"uri":"file:///path/to/basic_gen.go","version":10},"edits":[{"range":{"start":{"line":2,"character":1},"end":{"line":3,"character":2}},"newText":"foo bar"}]}`,
+				wantMarshalErr: false,
+				wantErr:        true,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				got, err := gojay.MarshalJSONObject(&tt.field)
+				if (err != nil) != tt.wantMarshalErr {
+					t.Error(err)
+					return
+				}
+
+				if diff := cmp.Diff(string(got), tt.want); (diff != "") != tt.wantErr {
+					t.Errorf("%s: wantErr: %t\n(-got, +want)\n%s", tt.name, tt.wantErr, diff)
+				}
+			})
+		}
+	})
+
+	t.Run("Unmarshal", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name             string
+			field            string
+			want             TextDocumentEdit
+			wantUnmarshalErr bool
+			wantErr          bool
+		}{
+			{
+				name:  "Valid",
+				field: `{"textDocument":{"uri":"file:///path/to/basic.go","version":10},"edits":[{"range":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"newText":"foo bar"}]}`,
+				want: TextDocumentEdit{
+					TextDocument: VersionedTextDocumentIdentifier{
+						TextDocumentIdentifier: TextDocumentIdentifier{
+							URI: "file:///path/to/basic.go",
+						},
+						Version: Uint64(10),
+					},
+					Edits: []TextEdit{
+						{
+							Range: Range{
+								Start: Position{
+									Line:      25,
+									Character: 1,
+								},
+								End: Position{
+									Line:      27,
+									Character: 3,
+								},
+							},
+							NewText: "foo bar",
+						},
+					},
+				},
+				wantUnmarshalErr: false,
+				wantErr:          false,
+			},
+			{
+				name:  "Invalid",
+				field: `{"textDocument":{"uri":"file:///path/to/basic.go","version":10},"edits":[{"range":{"start":{"line":25,"character":1},"end":{"line":27,"character":3}},"newText":"foo bar"}]}`,
+				want: TextDocumentEdit{
+					TextDocument: VersionedTextDocumentIdentifier{
+						TextDocumentIdentifier: TextDocumentIdentifier{
+							URI: "file:///path/to/basic.go",
+						},
+						Version: Uint64(10),
+					},
+					Edits: []TextEdit{
+						{
+							Range: Range{
+								Start: Position{
+									Line:      2,
+									Character: 1,
+								},
+								End: Position{
+									Line:      3,
+									Character: 2,
+								},
+							},
+							NewText: "foo bar",
+						},
+					},
+				},
+				wantUnmarshalErr: false,
+				wantErr:          true,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				got := TextDocumentEdit{}
+				dec := gojay.BorrowDecoder(strings.NewReader(tt.field))
+				defer dec.Release()
+				if err := dec.Decode(&got); (err != nil) != tt.wantUnmarshalErr {
+					t.Error(err)
+					return
+				}
+
+				if diff := cmp.Diff(got, tt.want); (diff != "") != tt.wantErr {
+					t.Errorf("%s: wantErr: %t\n(-got, +want)\n%s", tt.name, tt.wantErr, diff)
+				}
+			})
+		}
+	})
+}
