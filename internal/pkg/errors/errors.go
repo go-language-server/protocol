38d766784ad4b69c45528ba18c8eaa52281d3a21
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-08 10:46:43 +0900

    internal/pkg/errors: add internal/pkg/errors package

---
+++ internal/pkg/errors/errors.go
@@ -0,0 +1,14 @@
+// Copyright 2019 The go-language-server Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import (
+	"github.com/go-language-server/jsonrpc2"
+)
+
+// ErrorInvalidParams reports InvalidParams error.
+func ErrorInvalidParams(format string, args ...interface{}) error {
+	return jsonrpc2.Errorf(jsonrpc2.CodeInvalidParams, format, args...)
+}
