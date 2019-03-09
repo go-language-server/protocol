a5ea04c79e6f73935a4746b0107bb351286f9f37
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-09 09:36:26 +0900

    types: add stringSlice gojay implements method

--- types_gen.go
+++ types_gen.go
@@ -33,3 +33,29 @@ func (v *interfaces) MarshalJSONArray(enc *gojay.Encoder) {
 func (v *interfaces) IsNil() bool {
 	return &v == nil || len(*v) == 0
 }
+
+type stringSlice []string
+
+func (v *stringSlice) UnmarshalJSONArray(dec *gojay.Decoder) error {
+	t := ""
+	if err := dec.String(&t); err != nil {
+		return err
+	}
+	*v = append(*v, t)
+	return nil
+}
+
+// NKeys returns the number of keys to unmarshal.
+func (v *stringSlice) NKeys() int { return 1 }
+
+// MarshalJSONArray implements gojay's MarshalerJSONArray.
+func (v *stringSlice) MarshalJSONArray(enc *gojay.Encoder) {
+	for _, t := range *v {
+		enc.String(t)
+	}
+}
+
+// IsNil implements gojay's MarshalerJSONArray.
+func (v *stringSlice) IsNil() bool {
+	return &v == nil || len(*v) == 0
+}
