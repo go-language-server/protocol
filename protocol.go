f62a154b1acc3fdb66d8f56fce476715f6dd6b22
Koichi Shiraishi <zchee.io@gmail.com>
2019-04-07 11:39:20 +0900

    jsonrpc2: export Default{BufferSize,Canceler} and add godoc comment

--- protocol.go
+++ protocol.go
@@ -16,32 +16,24 @@ const (
 	Version = "3.14.0"
 )
 
-const defaultMessageBufferSize = 20
+// DefaultBufferSize default message buffer size.
+const DefaultBufferSize = 20
 
-func canceller(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) {
-	conn.Notify(context.Background(), "$/cancelRequest", &CancelParams{ID: *req.ID})
+// DefaultCanceller returns the default canceler function.
+func DefaultCanceller(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) {
+	conn.Notify(context.Background(), cancelRequest, &CancelParams{ID: *req.ID})
 }
 
-func NewServer(ctx context.Context, server Server, stream jsonrpc2.Stream, logger *zap.Logger) (*jsonrpc2.Conn, Client) {
-	opts := []jsonrpc2.Options{
-		jsonrpc2.WithHandler(serverHandler(server, logger)),
-		jsonrpc2.WithCanceler(jsonrpc2.Canceler(canceller)),
-		jsonrpc2.WithCapacity(defaultMessageBufferSize),
-		jsonrpc2.WithOverloaded(true),
-	}
+// NewServer returns the new jsonrpc2.Conn for Server and Client.
+func NewServer(ctx context.Context, server Server, stream jsonrpc2.Stream, logger *zap.Logger, opts ...jsonrpc2.Options) (*jsonrpc2.Conn, Client) {
 	conn := jsonrpc2.NewConn(ctx, stream, opts...)
 	c := &client{Conn: conn}
 
 	return conn, c
 }
 
-func NewClient(ctx context.Context, client Client, stream jsonrpc2.Stream, logger *zap.Logger) (*jsonrpc2.Conn, Server) {
-	opts := []jsonrpc2.Options{
-		jsonrpc2.WithHandler(clientHandler(client, logger)),
-		jsonrpc2.WithCanceler(jsonrpc2.Canceler(canceller)),
-		jsonrpc2.WithCapacity(defaultMessageBufferSize),
-		jsonrpc2.WithOverloaded(true),
-	}
+// NewClient returns the new jsonrpc2.Conn for Client and Server.
+func NewClient(ctx context.Context, client Client, stream jsonrpc2.Stream, logger *zap.Logger, opts ...jsonrpc2.Options) (*jsonrpc2.Conn, Server) {
 	conn := jsonrpc2.NewConn(ctx, stream, opts...)
 
 	return conn, &server{Conn: conn}
