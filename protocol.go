488ee04c820157b3069482527fbe3e508830e105
Koichi Shiraishi <zchee.io@gmail.com>
2019-04-07 10:22:06 +0900

    protocol: add more jsonrpc2 option to NewServer and NewClient

--- protocol.go
+++ protocol.go
@@ -22,17 +22,27 @@ func canceller(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) 
 	conn.Notify(context.Background(), "$/cancelRequest", &CancelParams{ID: *req.ID})
 }
 
-func NewServer(ctx context.Context, server Server, stream jsonrpc2.Stream, logger *zap.Logger) (*jsonrpc2.Conn, Client, *zap.Logger) {
-	conn := jsonrpc2.NewConn(ctx, stream, jsonrpc2.WithHandler(serverHandler(server, logger)), jsonrpc2.WithCanceler(jsonrpc2.Canceler(canceller)))
-	client := &clientDispatcher{Conn: conn}
-	// conn.Capacity = defaultMessageBufferSize
-	// conn.RejectIfOverloaded = true
-	return conn, client, logger
+func NewServer(ctx context.Context, server Server, stream jsonrpc2.Stream, logger *zap.Logger) (*jsonrpc2.Conn, Client) {
+	opts := []jsonrpc2.Options{
+		jsonrpc2.WithHandler(serverHandler(server, logger)),
+		jsonrpc2.WithCanceler(jsonrpc2.Canceler(canceller)),
+		jsonrpc2.WithCapacity(defaultMessageBufferSize),
+		jsonrpc2.WithOverloaded(true),
+	}
+	conn := jsonrpc2.NewConn(ctx, stream, opts...)
+	c := &client{Conn: conn}
+
+	return conn, c
 }
 
-func NewClient(ctx context.Context, client Client, stream jsonrpc2.Stream, logger *zap.Logger) (*jsonrpc2.Conn, Server, *zap.Logger) {
-	conn := jsonrpc2.NewConn(ctx, stream, jsonrpc2.WithHandler(clientHandler(client, logger)), jsonrpc2.WithCanceler(jsonrpc2.Canceler(canceller)))
-	// conn.Capacity = defaultMessageBufferSize
-	// conn.RejectIfOverloaded = true
-	return conn, &serverDispatcher{Conn: conn}, logger
+func NewClient(ctx context.Context, client Client, stream jsonrpc2.Stream, logger *zap.Logger) (*jsonrpc2.Conn, Server) {
+	opts := []jsonrpc2.Options{
+		jsonrpc2.WithHandler(clientHandler(client, logger)),
+		jsonrpc2.WithCanceler(jsonrpc2.Canceler(canceller)),
+		jsonrpc2.WithCapacity(defaultMessageBufferSize),
+		jsonrpc2.WithOverloaded(true),
+	}
+	conn := jsonrpc2.NewConn(ctx, stream, opts...)
+
+	return conn, &server{Conn: conn}
 }
