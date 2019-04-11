a6a04e7edde280cd4400b134b48c77871a946c64
Koichi Shiraishi <zchee.io@gmail.com>
2019-04-11 14:37:35 +0900

    client: rename Client to ClientInterface and export handler and Client

--- client.go
+++ client.go
@@ -12,8 +12,8 @@ import (
 	"go.uber.org/zap"
 )
 
-// Client represents a implementation of language-server-protocol client.
-type Client interface {
+// ClientInterface represents a implementation of language-server-protocol client.
+type ClientInterface interface {
 	RegisterCapability(ctx context.Context, params *RegistrationParams) (err error)
 	UnregisterCapability(ctx context.Context, params *UnregistrationParams) (err error)
 	Telemetry(ctx context.Context, params interface{}) (err error)
@@ -39,63 +39,63 @@ const (
 	workspaceWorkspaceFolders      = "workspace/workspaceFolders"
 )
 
-type client struct {
+type Client struct {
 	*jsonrpc2.Conn
 }
 
-var _ Client = (*client)(nil)
+var _ ClientInterface = (*Client)(nil)
 
-func (c *client) RegisterCapability(ctx context.Context, params *RegistrationParams) (err error) {
+func (c *Client) RegisterCapability(ctx context.Context, params *RegistrationParams) (err error) {
 	err = c.Conn.Notify(ctx, clientRegisterCapability, *params)
 	return
 }
 
-func (c *client) UnregisterCapability(ctx context.Context, params *UnregistrationParams) (err error) {
+func (c *Client) UnregisterCapability(ctx context.Context, params *UnregistrationParams) (err error) {
 	err = c.Conn.Notify(ctx, clientUnregisterCapability, *params)
 	return
 }
 
-func (c *client) Telemetry(ctx context.Context, params interface{}) (err error) {
+func (c *Client) Telemetry(ctx context.Context, params interface{}) (err error) {
 	err = c.Conn.Notify(ctx, telemetryEvent, params)
 	return
 }
 
-func (c *client) PublishDiagnostics(ctx context.Context, params *PublishDiagnosticsParams) (err error) {
+func (c *Client) PublishDiagnostics(ctx context.Context, params *PublishDiagnosticsParams) (err error) {
 	err = c.Conn.Notify(ctx, textDocumentPublishDiagnostics, *params)
 	return
 }
 
-func (c *client) LogMessage(ctx context.Context, params *LogMessageParams) (err error) {
+func (c *Client) LogMessage(ctx context.Context, params *LogMessageParams) (err error) {
 	err = c.Conn.Notify(ctx, windowLogMessage, *params)
 	return
 }
 
-func (c *client) ShowMessage(ctx context.Context, params *ShowMessageParams) (err error) {
+func (c *Client) ShowMessage(ctx context.Context, params *ShowMessageParams) (err error) {
 	err = c.Conn.Notify(ctx, windowShowMessage, *params)
 	return
 }
 
-func (c *client) ShowMessageRequest(ctx context.Context, params *ShowMessageRequestParams) (result *MessageActionItem, err error) {
+func (c *Client) ShowMessageRequest(ctx context.Context, params *ShowMessageRequestParams) (result *MessageActionItem, err error) {
 	result = new(MessageActionItem)
 	err = c.Conn.Call(ctx, windowShowMessageRequest, *params, result)
 
 	return result, err
 }
 
-func (c *client) WorkspaceApplyEdit(ctx context.Context, params *ApplyWorkspaceEditParams) (result bool, err error) {
+func (c *Client) WorkspaceApplyEdit(ctx context.Context, params *ApplyWorkspaceEditParams) (result bool, err error) {
 	err = c.Conn.Call(ctx, workspaceApplyEdit, *params, &result)
 
 	return result, err
 }
 
-func (c *client) WorkspaceConfiguration(ctx context.Context, params *ConfigurationParams) (_ []interface{}, err error) {
+func (c *Client) WorkspaceConfiguration(ctx context.Context, params *ConfigurationParams) (_ []interface{}, err error) {
 	var result []interface{}
 	err = c.Conn.Call(ctx, workspaceConfiguration, *params, &result)
 
 	return result, err
 }
 
-func (c *client) WorkspaceFolders(ctx context.Context) (result []WorkspaceFolder, err error) {
+func (c *Client) WorkspaceFolders(ctx context.Context) (result []WorkspaceFolder, err error) {
 	result = []WorkspaceFolder{}
 	err = c.Conn.Call(ctx, workspaceWorkspaceFolders, nil, &result)
 
@@ -103,12 +103,12 @@ func (c *client) WorkspaceFolders(ctx context.Context) (result []WorkspaceFolder
 }
 
 // ClientHandler returns the client handler.
-func ClientHandler(client Client, logger *zap.Logger) jsonrpc2.Handler {
+func ClientHandler(client ClientInterface, logger *zap.Logger) jsonrpc2.Handler {
 	return func(ctx context.Context, conn *jsonrpc2.Conn, r *jsonrpc2.Request) {
 		switch r.Method {
 		case cancelRequest:
 			var params CancelParams
-			if err := gojay.Unsafe.Unmarshal(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.Unmarshal(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
@@ -116,70 +116,77 @@ func ClientHandler(client Client, logger *zap.Logger) jsonrpc2.Handler {
 
 		case clientRegisterCapability:
 			var params RegistrationParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			if err := client.RegisterCapability(ctx, &params); err != nil {
 				logger.Error(clientRegisterCapability, zap.Error(err))
 			}
 
 		case clientUnregisterCapability:
 			var params UnregistrationParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			if err := client.UnregisterCapability(ctx, &params); err != nil {
 				logger.Error(clientUnregisterCapability, zap.Error(err))
 			}
 
 		case telemetryEvent:
 			var params interface{}
-			if err := gojay.Unsafe.Unmarshal(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.Unmarshal(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			if err := client.Telemetry(ctx, &params); err != nil {
 				logger.Error(telemetryEvent, zap.Error(err))
 			}
 
 		case textDocumentPublishDiagnostics:
 			var params PublishDiagnosticsParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			if err := client.PublishDiagnostics(ctx, &params); err != nil {
 				logger.Error(textDocumentPublishDiagnostics, zap.Error(err))
 			}
 
 		case windowLogMessage:
 			var params LogMessageParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			if err := client.LogMessage(ctx, &params); err != nil {
 				logger.Error(windowLogMessage, zap.Error(err))
 			}
 
 		case windowShowMessage:
 			var params ShowMessageParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			if err := client.ShowMessage(ctx, &params); err != nil {
 				logger.Error(windowShowMessage, zap.Error(err))
 			}
 
 		case windowShowMessageRequest:
 			var params ShowMessageRequestParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			resp, err := client.ShowMessageRequest(ctx, &params)
 			if err := conn.Reply(ctx, r, resp, err); err != nil {
 				logger.Error(windowShowMessageRequest, zap.Error(err))
@@ -187,10 +194,11 @@ func ClientHandler(client Client, logger *zap.Logger) jsonrpc2.Handler {
 
 		case workspaceApplyEdit:
 			var params ApplyWorkspaceEditParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			resp, err := client.WorkspaceApplyEdit(ctx, &params)
 			if err := conn.Reply(ctx, r, resp, err); err != nil {
 				logger.Error(workspaceApplyEdit, zap.Error(err))
@@ -198,10 +206,11 @@ func ClientHandler(client Client, logger *zap.Logger) jsonrpc2.Handler {
 
 		case workspaceConfiguration:
 			var params ConfigurationParams
-			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params.EmbeddedJSON, &params); err != nil {
+			if err := gojay.Unsafe.UnmarshalJSONObject(*r.Params, &params); err != nil {
 				ReplyError(ctx, err, conn, r, logger)
 				return
 			}
+
 			resp, err := client.WorkspaceConfiguration(ctx, &params)
 			if err := conn.Reply(ctx, r, resp, err); err != nil {
 				logger.Error(workspaceConfiguration, zap.Error(err))
@@ -212,6 +221,7 @@ func ClientHandler(client Client, logger *zap.Logger) jsonrpc2.Handler {
 				conn.Reply(ctx, r, nil, jsonrpc2.Errorf(jsonrpc2.CodeInvalidParams, "Expected no params"))
 				return
 			}
+
 			resp, err := client.WorkspaceFolders(ctx)
 			if err := conn.Reply(ctx, r, resp, err); err != nil {
 				logger.Error(workspaceWorkspaceFolders, zap.Error(err))
