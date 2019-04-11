669db2f1df4e61b0ae6a735bfbc67c79064e02b0
Koichi Shiraishi <zchee.io@gmail.com>
2019-04-11 22:32:02 +0900

    protocol: add emptyData constant

---
+++ protocol_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The go-language-server Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package protocol
+
+const emptyData = `{}`
