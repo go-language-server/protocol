fedd7b778b5c4a161ddbb76fd81c43c69eef7cde
Koichi Shiraishi <zchee.io@gmail.com>
2019-04-24 09:14:02 +0900

    internal/gojaypool: add gojaypool package for wrapped BorrowDecoder func

---
+++ internal/gojaypool/gojaypool.go
@@ -0,0 +1,23 @@
+// Copyright 2019 The go-language-server Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gojaypool
+
+import (
+	"io"
+	_ "unsafe" // required for go:linkname
+
+	"github.com/francoispqt/gojay"
+)
+
+//go:linkname borrowDecoder github.com/francoispqt/gojay.borrowDecoder
+func borrowDecoder(r io.Reader, bufSize int) *gojay.Decoder
+
+// BorrowSizedDecoder borrows a Decoder from the pool.
+// It takes an io.Reader implementation as data input.
+//
+// In order to benefit from the pool, a borrowed decoder must be released after usage.
+func BorrowSizedDecoder(r io.Reader, bufSize int) *gojay.Decoder {
+	return borrowDecoder(r, bufSize)
+}
---
+++ internal/gojaypool/gojaypool.s
@@ -0,0 +1,7 @@
+// Copyright 2019 The go-language-server Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The presence of this file allows the package to use the
+// "go:linkname" hack to call non-exported functions in the
+// Go runtime.
