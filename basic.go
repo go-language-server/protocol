ac9176c5961d25699ddcda20b612a52ef2135c84
Koichi Shiraishi <zchee.io@gmail.com>
2020-03-09 08:07:55 +0900

    all: change license author

--- basic.go
+++ basic.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The go-language-server Authors. All rights reserved.
+// Copyright 2019 The Go Language Server Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
