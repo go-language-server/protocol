92acdfed5b9b5e0d236a3621ef80ecf441f811ee
Anthony Bullard <anthony.bullard@gmail.com>
2024-01-01 11:16:26 -0600

    Add documentation comments

--- capabilities_server.go
+++ capabilities_server.go
@@ -428,6 +428,7 @@ type LinkedEditingRangeRegistrationOptions struct {
 
 // SemanticTokensFullOptions option of SemanticTokensOptions to indicate support for full and/or full with delta semantic tokens requests.
 type SemanticTokensFullOptions struct {
+    // The server supports deltas for full documents.
 	Delta bool `json:"delta"`
 }
 
@@ -436,8 +437,11 @@ type SemanticTokensFullOptions struct {
 // @since 3.16.0.
 type SemanticTokensOptions struct {
 	WorkDoneProgressOptions
+    // The legend used by the server
 	Legend *SemanticTokensLegend `json:"legend,omitempty"`
+    // Server supports providing semantic tokens for a specific range of a document.
 	Range  bool                  `json:"range"`
+    // Server supports providing semantic tokens for a full document.
 	Full   interface{}           `json:"full,omitempty"`
 }
 
